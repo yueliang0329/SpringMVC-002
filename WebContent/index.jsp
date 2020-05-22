52d3de3b4d13dd64517929a8304c81be4aad01d6
yuel_global <13373657935_global@163.com>
2020-05-22 15:04:11 +0800

    local

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -28,5 +28,29 @@
 		<br><br>
 		<a href="testJson" id="testJson">Test Json</a>
 		<br><br>
+		
+		<form action="testHttpMessageConverter" method="POST" enctype="multipart/form-data">
+			File: <input type="file" name="file"/>
+			Desc: <input type="text" name="desc"/>
+			<input type="submit" value="Submit"/>
+		</form>
+		
+		<br><br>
+		<a href="testResponseEntity">Test ResponseEntity</a>
+		
+		<!--  
+		关于国际化:
+		1. 在页面上能够根据浏览器语言设置的情况对文本(不是内容), 时间, 数值进行本地化处理
+		2. 可以在 bean 中获取国际化资源文件 Locale 对应的消息
+		3. 可以通过超链接切换 Locale, 而不再依赖于浏览器的语言设置情况
+		
+		解决:
+		1. 使用 JSTL 的 fmt 标签
+		2. 在 bean 中注入 ResourceBundleMessageSource 的示例, 使用其对应的 getMessage 方法即可
+		3. 配置 LocalResolver 和 LocaleChangeInterceptor
+	-->	
+	
+		<br><br>
+		<a href="i18n">I18N PAGE</a>
 </body>
 </html>
--- src/com/gzj/springmvc/test/SpringMVCTest.java
+++ src/com/gzj/springmvc/test/SpringMVCTest.java
@@ -1,8 +1,21 @@
 package com.gzj.springmvc.test;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Collection;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,6 +29,9 @@ public class SpringMVCTest {
 
 	@Autowired
 	private EmployeeDao employeeDao;
+	
+	@Autowired
+	private ResourceBundleMessageSource messageSource;
 	
 	@RequestMapping("/testConversionServiceConverer")
 	public String testConverter(@RequestParam("employee") Employee employee){
@@ -31,4 +47,33 @@ public class SpringMVCTest {
 		return employeeDao.getAll();
 	}
 	
+	@ResponseBody
+	@RequestMapping("/testHttpMessageConverter")
+	public String testHttpMessageConverter(@RequestBody String body){
+		System.out.println(body);
+		return "helloworld! " + new Date();
+	}
+	
+	@RequestMapping("/testResponseEntity")
+	public ResponseEntity<byte[]> testResponseEntity(HttpSession session) throws IOException
+	{
+		byte [] body =null ;
+		ServletContext servletContext = session.getServletContext();
+		InputStream in = servletContext.getResourceAsStream("/files/abc.txt");
+		body = new byte[in.available()];
+		in.read(body);
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-Disposition", "attachment;filename=abc.txt");	
+		HttpStatus statusCode = HttpStatus.OK;
+		ResponseEntity<byte[]> response = new ResponseEntity<byte[]>(body, headers, statusCode);
+		return response;
+	}
+	
+	@RequestMapping("/i18n")
+	public String testI18n(Locale locale){
+		String val = messageSource.getMessage("i18n.user", null, locale);
+		System.out.println(val); 
+		return "i18n";
+	}
 }
